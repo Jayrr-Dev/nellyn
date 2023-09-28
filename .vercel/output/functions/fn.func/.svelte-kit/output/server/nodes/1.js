1084b827a0d7011af2cd1facbe0c36fb879113ea
Samuel <samuel.v.soriano@gmail.com>
2023-09-28 13:50:46 -0600

    fix 2

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.bfc6999b.js","_app/immutable/chunks/index.55486355.js","_app/immutable/chunks/singletons.a2682834.js"];
+export const imports = ["_app/immutable/nodes/1.e81965f4.js","_app/immutable/chunks/index.55486355.js","_app/immutable/chunks/singletons.3d9cb263.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.ico"]),
 	mimeTypes: {".ico":"image/vnd.microsoft.icon"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.30b95026.js","app":"_app/immutable/entry/app.c40ace90.js","imports":["_app/immutable/entry/start.30b95026.js","_app/immutable/chunks/index.55486355.js","_app/immutable/chunks/singletons.a2682834.js","_app/immutable/entry/app.c40ace90.js","_app/immutable/chunks/index.55486355.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ffd2dbfd.js","app":"_app/immutable/entry/app.2bd3a4a7.js","imports":["_app/immutable/entry/start.ffd2dbfd.js","_app/immutable/chunks/index.55486355.js","_app/immutable/chunks/singletons.3d9cb263.js","_app/immutable/entry/app.2bd3a4a7.js","_app/immutable/chunks/index.55486355.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
